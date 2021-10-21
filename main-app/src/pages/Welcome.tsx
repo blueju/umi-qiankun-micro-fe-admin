0e6e0e67871c607625803368cedfd14e55433533
blueju <blueju@foxmail.com>
2021-10-21 10:13:21 +0800

    做了以下修改： 1.css in js，使用 antd color var 设置 style color

--- main-app/src/pages/Welcome.tsx
+++ main-app/src/pages/Welcome.tsx
@@ -1,33 +1,54 @@
 // @ts-ignore
 import { history } from 'umi';
 import { noop } from 'lodash';
-import { Card, Avatar, Popover, Tooltip, Row, Col } from 'antd';
 import React, { useEffect, useState } from 'react';
+import { Card, Avatar, Popover, Tooltip, Row, Col } from 'antd';
 import {
-  LoginOutlined,
+  LoadingOutlined,
   InfoCircleOutlined,
   CheckCircleOutlined,
   CloseCircleOutlined,
-  WarningOutlined,
 } from '@ant-design/icons';
 
-import { mainAppName } from '@/utils/utils';
 import { IApp } from '../../mock/apps';
+// @ts-ignore
+import styles from './Welcome.less'
+import { mainAppName } from '@/utils/utils';
 
 const { Meta } = Card;
 const { apps } = JSON.parse(localStorage.getItem(mainAppName));
 
 /* 扩展了连通性状态的 app interface */
-interface IAppWithConnectable extends IApp {
-  connectable: boolean;
+interface IAppWithConnectionStatus extends IApp {
+  connectionStatus: 'pending' | 'resolve' | 'reject';
 }
 
+/* app 连接状态的 interface */
+interface IAppConnectionStatus {
+  /* 等待中 */
+  pending: 'pending';
+  /* 连接正常 */
+  resolve: 'resolve';
+  /* 连接异常 */
+  reject: 'reject';
+}
+
+/* app 连接状态 */
+const appConnectionStatus: IAppConnectionStatus = {
+  pending: 'pending',
+  resolve: 'resolve',
+  reject: 'reject',
+};
+
 export default () => {
-  const [appsWithConnectable, setAppsWithConnectable] = useState<IAppWithConnectable[]>(
+  // 带 app 连接状态的应用信息列表
+  const [appsWithConnectionStatus, setAppsWithConnectionStatus] = useState<
+    IAppWithConnectionStatus[]
+  >(
     apps.map((item) => {
       return {
         ...item,
-        connectable: false,
+        connectionStatus: 'pending',
       };
     }),
   );
@@ -42,58 +63,65 @@ export default () => {
 
   /* 检测应用连通性 */
   function checkAppConnection() {
-    const connectAppsPromiseList: Promise<Response>[] = appsWithConnectable.map((item) => {
+    const connectAppsPromiseList: Promise<Response>[] = appsWithConnectionStatus.map((item) => {
       return fetch(item.entry);
     });
-    const newAppsWithConnectable = [...appsWithConnectable];
+    const newAppsWithConnectionStatus = [...appsWithConnectionStatus];
     // @ts-ignore
     Promise.allSettled(connectAppsPromiseList).then((res) => {
       res.forEach((item, index) => {
-        newAppsWithConnectable[index].connectable = item.status === 'fulfilled';
+        newAppsWithConnectionStatus[index].connectionStatus =
+          item.status === 'fulfilled' ? appConnectionStatus.resolve : appConnectionStatus.reject;
       });
-      setAppsWithConnectable(newAppsWithConnectable);
+      setAppsWithConnectionStatus(newAppsWithConnectionStatus);
     });
   }
 
   /**
    * 渲染应用可连通性图标
-   * @param connectable 是否可连通
+   * @param connectionStatus 连通状态
    */
-  function renderAppConnectableIcon(connectable) {
-    return connectable ? (
-      <CheckCircleOutlined style={{ color: '#52c41a' }} />
-    ) : (
-      <CloseCircleOutlined style={{ color: '#fe4d4f' }} />
-    );
+  function renderAppConnectionStatusIcon(connectionStatus) {
+    switch (connectionStatus) {
+      case appConnectionStatus.pending:
+        return <LoadingOutlined />;
+      case appConnectionStatus.resolve:
+        return <CheckCircleOutlined style={{ color: styles.successColor }} />;
+      case appConnectionStatus.reject:
+        return <CloseCircleOutlined style={{ color: styles.errorColor }} />;
+      default:
+        return <LoadingOutlined />;
+    }
   }
 
   /**
    * 渲染应用入口图标
-   * @param connectable 是否可连通
+   * @param connectionStatus 连通状态
    * @param homepage    应用首页
    */
-  function renderAppEntryIcon(connectable, homepage) {
-    return connectable ? (
-      <LoginOutlined key="login" onClick={connectable ? () => toSubApp(homepage) : noop} />
-    ) : (
-      <WarningOutlined style={{ color: '#faad14' }} />
-    );
+  function renderAppEntryIcon(connectionStatus, homepage) {
+    return connectionStatus === appConnectionStatus.resolve ? (
+      <a onClick={connectionStatus ? () => toSubApp(homepage) : noop}>进入</a>
+    ) : null;
   }
 
   function renderAppCard() {
-    return appsWithConnectable.map((item) => {
+    return appsWithConnectionStatus.map((item) => {
       return (
         <Col span={8} key={item.name}>
           <Card
             style={{ marginRight: 24, marginBottom: 24 }}
             actions={[
-              <Tooltip key="appConnectable" title={`应用连通${item.connectable ? '正常' : '异常'}`}>
-                <div>{renderAppConnectableIcon(item.connectable)}</div>
+              <Tooltip
+                key="appConnectionStatus"
+                title={`应用连通${item.connectionStatus ? '正常' : '异常'}`}
+              >
+                <div>{renderAppConnectionStatusIcon(item.connectionStatus)}</div>
               </Tooltip>,
               <Popover
                 key="appInfo"
                 title="应用信息"
-                placement="topRight"
+                placement="top"
                 arrowPointAtCenter
                 content={
                   <>
@@ -106,8 +134,8 @@ export default () => {
               >
                 <InfoCircleOutlined />
               </Popover>,
-              <Tooltip key="appEntry" title={`应用${item.connectable ? '' : '不'}可访问`}>
-                <div>{renderAppEntryIcon(item.connectable, item.homepage)}</div>
+              <Tooltip key="appEntry" title={`应用${item.connectionStatus ? '' : '不'}可访问`}>
+                <div>{renderAppEntryIcon(item.connectionStatus, item.homepage)}</div>
               </Tooltip>,
             ]}
           >
