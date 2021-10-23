fee6684f244e4d3fc21c1675a7b50fe13b29d551
blueju <blueju@foxmail.com>
2021-10-23 10:33:35 +0800

    删除从codesandbox创建的react工程，改为create-react-app创建的react工程

--- sub-app-react/src/App.js
+++ sub-app-react/src/App.js
@@ -1,10 +1,25 @@
-import "./styles.css";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
